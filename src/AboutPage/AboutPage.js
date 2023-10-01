e6bd196a9587e8c0426447cc18ce6541c1d5da41
ramya@RJampani_4 <rjampan1@uncc.edu>
2023-10-01 00:25:32 -0400

    pbreact2

---
+++ src/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div >
+       <p> Ramya Jampani's Personal Budget </p>
+      <p> React</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+    <div className="bottom">
+        <div className="center">
+            All rights reserved &copy; Ramya Jampani
+        </div>
+    </div>
+</footer>   
+  );
+}
+
+export default Footer;
